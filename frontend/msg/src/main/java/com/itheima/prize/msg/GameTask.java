9c8c0e8eba503220c0b238302706799a7c476dcc
Blue <3067670134@qq.com>
2025-11-23 23:39:27 +0800

    活动逻辑

--- frontend/msg/src/main/java/com/itheima/prize/msg/GameTask.java
+++ frontend/msg/src/main/java/com/itheima/prize/msg/GameTask.java
@@ -8,6 +8,7 @@ import com.itheima.prize.commons.db.service.CardGameRulesService;
 import com.itheima.prize.commons.db.service.CardGameService;
 import com.itheima.prize.commons.db.service.GameLoadService;
 import com.itheima.prize.commons.utils.RedisUtil;
+import io.swagger.models.auth.In;
 import org.apache.commons.lang3.time.DateUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -38,6 +39,65 @@ public class GameTask {
     @Scheduled(cron = "0 * * * * ?")
     public void execute() {
         System.out.printf("scheduled!"+new Date());
-        //TODO
-    }
+        //查询一分钟内的活动
+        QueryWrapper<CardGame> gameQueryWrapper = new QueryWrapper<>();
+        Date now=new Date();
+        gameQueryWrapper.gt("starttime",now);  gameQueryWrapper.le("starttime",DateUtils.addMinutes(now,1));
+        List<CardGame> list = gameService.list(gameQueryWrapper);
+
+        if(list==null||list.size()==0){
+            log.info("game list scan : size = 0");
+            return;
+        }
+        log.info("game list scan : size = {}",list.size());
+        //遍历列表处理
+        for (CardGame game : list) {
+
+            long begin = game.getStarttime().getTime();
+            long end = game.getEndtime().getTime();
+            //活动总时间
+            long duration = end - begin;
+            //过期时间（活动剩余时间）
+            long expire = (end - now.getTime()) / 1000;
+            Map queryMap = new HashMap();
+            queryMap.put("gameid",game.getId());
+
+            //活动基本信息
+            game.setStatus(1);
+            redisUtil.set(RedisKeys.INFO+game.getId(),game,-1);
+            log.info("load game info:{}{}{}{}",game.getId(),game.getTitle(),game.getStarttime(),game.getEndtime());
+
+            //奖品基本信息
+            List<CardProductDto> products = gameLoadService.getByGameId(game.getId());
+            HashMap<Integer, CardProductDto> productsMap = new HashMap<>(products.size());
+            for (CardProductDto p : products) {
+                productsMap.put(p.getId(),p);
+            }
+            log.info("load product type:{}",products.size());
+
+            //奖品数量等配置信息
+            List<CardGameProduct> gameProducts = gameProductService.listByMap(queryMap);
+            log.info("load bind product:{}",gameProducts.size());
+
+            //令牌桶
+            List<Long> tokenList = new ArrayList();
+            for (CardGameProduct product : gameProducts) {
+                for(int i=0;i<product.getAmount();i++){
+                    long rnd = begin + new Random().nextInt((int) duration);
+                    Long token=rnd*1000+new Random().nextInt(1000);
+                    tokenList.add(token);
+                    log.info("token -> game : {} -> {}",token/1000 ,productsMap.get(product.getProductid()).getName());
+                    redisUtil.set(RedisKeys.TOKEN+product.getGameid()+"_"+token,productsMap.get(product.getProductid()),expire);
+                }
+            }
+            Collections.sort(tokenList);
+            log.info("load tkoens:{}",tokenList);
+
+            //从右侧压入队列
+            redisUtil.rightPushAll(RedisKeys.TOKENS + game.getId(),tokenList);
+            redisUtil.expire(RedisKeys.TOKENS + game.getId(),expire);
+
+            CardGameRules
+
+        }
 }
